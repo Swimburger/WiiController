8edfdc99a7726d12e72aac44aafa7bfc1ac7a7b0
Niels Swimberghe <nielsswimberghe@gmail.com>
2014-03-28 09:48:14 +0100

    Init commit

---
+++ WiiLib/Wii.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WII.HID.Lib;
+
+namespace WiiLib
+{
+    public class Wii
+    {
+       
+    }
+}
